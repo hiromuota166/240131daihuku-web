d1eaa2949b123871098347d69ea92c4d879f09ca
hiromuota166 <otahiromu166@gmail.com>
2024-01-31 10:09:56 +0900

    カレンダーとヘッダーの登録

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,15 @@
 'use client';
-import {
-  BellFilled,
-  CaretDownOutlined,
-  FormOutlined,
-  UserOutlined,
-} from '@ant-design/icons/lib/icons';
-import { Avatar, Col, Input, Row, Typography } from 'antd';
+import React from 'react';
+import type { Dayjs } from 'dayjs';
+import { Calendar } from 'antd';
+import type { CalendarProps } from 'antd';
 
-const { Title, Text } = Typography;
+const App: React.FC = () => {
+  const onPanelChange = (value: Dayjs, mode: CalendarProps<Dayjs>['mode']) => {
+    console.log(value.format('YYYY-MM-DD'), mode);
+  };
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <div>
-          <Title>Qiita</Title>
-          <div>
-            <CaretDownOutlined />
-            <Input placeholder='キーワードを入力' />
-          </div>
-          <div>
-            <div>
-              <FormOutlined />
-              <Text>
-                投稿する
-              </Text>
-            </div>
-            <div>
-              <BellFilled />
-            </div>
-            <Avatar size='large' icon={<UserOutlined />} />
-          </div>
-        </div>
-      </header>
-    </div>
-  );
-}
+  return <Calendar onPanelChange={onPanelChange} />;
+};
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='w-auto'>
+      hello header
+    </div>
+  )
+}
+
+export default Header
