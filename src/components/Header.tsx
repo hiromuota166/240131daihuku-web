a11e13c85e46d8fdde50f7eb98b239782d7b034b
hiromuota166 <otahiromu166@gmail.com>
2024-02-03 01:15:54 +0900

    storybook導入

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,12 @@
 import React from 'react'
 
 function Header() {
+  const daihuku = 'Daihuku'
   return (
-    <div className='w-auto'>
-      hello header
+    <div className=''>
+      <h1>
+        {daihuku}
+      </h1>
     </div>
   )
 }
