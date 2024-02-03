ffadaac8544e320253d5eb4f275d8915d45946e7
hiromuota166 <otahiromu166@gmail.com>
2024-02-03 17:36:50 +0900

    Headerのicon読み込み成功

--- src/components/HeaderUI.tsx
+++ src/components/HeaderUI.tsx
@@ -2,17 +2,12 @@
 import React from 'react'
 import Image from 'next/image'
 
-interface HeaderUIProps {
-  login: boolean;
-}
-
-function HeaderUI(props: HeaderUIProps) {
-  const { login } = props
+function HeaderUI() {
   const daihuku = 'Daihuku'
   return (
     <div className=''>
       <Image
-          src="/cooking-header.svg"
+          src="/cooking-header.svg" //storybookだと何故か読み込みできない
           width={32}
           height={32}
           alt="ヘッダーアイコン"
@@ -21,21 +16,6 @@ function HeaderUI(props: HeaderUIProps) {
       <h1>
         {daihuku}
       </h1>
-      <div>
-        {login ? (
-          <>
-            <span className="welcome">
-              Welcome, <b>user.name</b>!
-            </span>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Log out</button>
-          </>
-        ) : (
-          <>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Log in</button>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Sign up</button>
-          </>
-        )}
-        </div>
     </div>
   )
 }
