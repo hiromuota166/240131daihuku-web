3a3fc1aa2cef297d5ac8b863ef375319c51c01bb
hiromuota166 <otahiromu166@gmail.com>
2024-02-03 17:56:42 +0900

    一旦置いておく

--- src/components/HeaderUI.tsx
+++ src/components/HeaderUI.tsx
@@ -5,15 +5,16 @@ import Image from 'next/image'
 function HeaderUI() {
   const daihuku = 'Daihuku'
   return (
-    <div className=''>
+    <div className='flex bg-teal-50'>
       <Image
           src="/cooking-header.svg" //storybookだと何故か読み込みできない
           width={32}
           height={32}
           alt="ヘッダーアイコン"
-          className="w-10 mt-5"
+          className="w-8 mt-5 m-2.5"
         />
-      <h1>
+      <h1
+        className='flex justify-start w-screen text-2xl items-end pb-2.5'>
         {daihuku}
       </h1>
     </div>
